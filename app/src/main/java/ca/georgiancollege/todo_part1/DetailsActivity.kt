f6856ca8cbec73c8bc15170a68de1e3af21bc320
DainShin <di.shin0611@gmail.com>
2024-07-17 22:35:23 -0400

    added click event for the calendarview

--- app/src/main/java/ca/georgiancollege/todo_part1/DetailsActivity.kt
+++ app/src/main/java/ca/georgiancollege/todo_part1/DetailsActivity.kt
@@ -1,6 +1,8 @@
 package ca.georgiancollege.todo_part1
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import ca.georgiancollege.todo_part1.databinding.ActivityDetailsBinding
 
@@ -12,6 +14,21 @@ class DetailsActivity: AppCompatActivity()
         super.onCreate(savedInstanceState)
         binding = ActivityDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
-    }
 
+        // hide calendarView by default
+        binding.calendarView.visibility = View.GONE
+
+        // click event
+        binding.dueDateSection.setOnClickListener {
+            Log.i("달력", "달력 클릭")
+            // Toggle visibility of CalendarView
+            if (binding.calendarView.visibility == View.GONE) {
+                binding.calendarView.visibility = View.VISIBLE
+                Log.i("달력", "달력 보이기")
+            } else {
+                binding.calendarView.visibility = View.GONE
+                Log.i("달력", "달력 숨기기")
+            }
+        }
+    }
 }
